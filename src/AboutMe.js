66cc4348760628395d79031936bd7c942b8a2745
marjames98 <mar.james1998@gmail.com>
2021-11-09 15:57:49 -0500

    addedpics

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -6,6 +6,12 @@ const aboutMe =
         <h1 class="Blazing" contenteditable="true">JAYFACTSMUSIC</h1>
 </div>  
 
+<div class="pictures">
+    <img class= "pics" src= "https://allmythology.com/wp-content/uploads/2019/11/greek-mythology-creature-empusae.jpg.webp"></img>
+    <img class= "pics" src= "https://static.displate.com/857x1200/displate/2020-06-09/e7199eba23b53a5dbf84e0fcc1db4234_8c23a032e77ad6c98e2012aa601e53f4.jpg"></img>
+    <img class= "pics" src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRbWOa2WkzSNLuYEoTjMqZsxGdKE3weBymyg&usqp=CAU"></img>
+</div>
+
 
 <script>
 document.getElementById("demo").innerHTML = 5 + 6;
